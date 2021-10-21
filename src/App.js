bc9aeecba853aa677370dea0d4dd16bd86d957fa
jonim-git <n0majo01@students.oamk.fi>
2021-10-21 13:36:19 +0300

    delete valmis

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
 
-import './App.css';
 import { useState, useEffect } from 'react'
 import axios from 'axios';
 
-const URL = 'http://localhost/todo/';
+const URL = 'http://localhost/shopping-list/';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [task, setTask] = useState('');
-  const [editTask, setEditTask] = useState(null);
+  const [items, setItems] = useState([]);
+  const [item, setItem] = useState('');
+  // const [description, setDescription] = useState('')
+  const [amount, setAmount] = useState(0)
+  const [editItem, setEditItem] = useState(null);
   const [editDescription, setEditDescription] = useState('');
+  const [editAmount, setEditAmount] = useState(0)
 
   useEffect(() => {
     axios.get(URL + 'index.php')
       .then((response) => {
-        console.log(response.data)
-        setTasks(response.data)
+        
+        setItems
+          (response.data)
       }).catch(error => {
         alert(error.response ? error.response.data.error : error);
       })
   }, [])
 
-  function save(e) {
+  function add(e) {
     e.preventDefault();
-    const json = JSON.stringify({ description: task })
+    const json = JSON.stringify({description : item, amount : amount})
     axios.post(URL + 'save.php', json, {
       headers: {
-        'Content-Type': 'applicationJ/json'
+        'Content-Type': 'application/json'
       }
+      
     })
       .then((response) => {
-        setTasks(tasks => [...tasks, response.data]);
-        setTask('');
+        setItems
+          (items => [...items, response.data]);
+        setItem('')
+        setAmount('');
       }).catch(error => {
         alert(error.response.data.error)
       })
@@ -45,64 +51,56 @@ function App() {
       }
     })
       .then((response) => {
-        const newListWithoutRemoved = tasks.filter((item) => item.id !== id);
-        setTasks(newListWithoutRemoved);
+        const newListWithoutRemoved = items.filter((item) => item.id !== id);
+        setItems(newListWithoutRemoved)
       }).catch(error => {
-        alert(error.response.data.error)
+        alert(error.response ? error.response.data.error : error);
       })
   }
 
-  function setEditedTask(task) {
-    setEditTask(task);
-    setEditDescription(task?.description);
+  function setEditedItem(item) {
+    setEditItem(item);
+    setEditDescription(item?.description)
+    setEditAmount(item?.amount);
   }
 
   function update(e) {
     e.preventDefault();
-    const json = JSON.stringify({id:editTask.id,description:editDescription})
-    axios.post(URL + 'update.php',json, {
+    const json = JSON.stringify({ id: editItem.id, description: editDescription, amount: editAmount })
+    axios.post(URL + 'update.php', json, {
       headers: {
-        'Content-Type' : 'application/json'
+        'Content-Type': 'application/json'
       }
     })
-    .then((response) => {
-      tasks[(tasks.findIndex(task => task.id === editTask.id))].description = editDescription;
-      setTasks([...tasks]);
-      setEditedTask(null);
-    }).catch (error => {
-      alert(error.response ? error.response.data.error : error);
-    })
+      .then((response) => {
+        items[(items.findIndex(item => item.id === editItem.id))].description = editDescription;
+        items[(items.findIndex(item => item.id === editItem.id))].amount = editAmount;
+        setItems
+          ([...items]);
+        setEditedItem(null);
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      })
   }
+
 
   return (
     <div className="container">
-      <h3>Todo list</h3>
-      <form onSubmit={save}>
-        <label>New task</label>
-        <input value={task} onChange={e => setTask(e.target.value)} />
+      <h3>Shopping list</h3>
+      <form onSubmit={add}>
+        <label>New item</label>
+        <input value={item} placeholder="type product" onChange={e => setItem(e.target.value)} />
+        <input value={amount} placeholder = "type amount" onChange={e => setAmount(e.target.value)} />
         <button>Save</button>
       </form>
       <ol>
-        {tasks?.map(task => (
-          <li key={task.id}>
-            {editTask?.id !== task.id &&
-              task.description
-            }
-            {editTask?.id === task.id &&
-              <form onSubmit={update}>
-                <input value={editDescription} onChange={e => setEditDescription(e.target.value)} />
-                <button>Save</button>
-                <button type="button" onClick={() => setEditedTask(null)}>Cancel</button>
-              </form>
-            }
-            <a href="#" className="delete" onClick={() => remove(task.id)}>
-               Delete
-            </a>&nbsp;
-            {editTask === null &&
-              <a className="edit" onClick={() => setEditedTask(task)} href="#">
-                 Edit
-              </a>
-            }
+        {items?.map(item => (
+          <li key={item.id}>
+            <span className ="desc">{item.description}</span>
+            <span className ="amount">{item.amount} pieces</span>
+            <button className="delete" onClick={() => remove(item.id)}>
+              Delete
+            </button>
           </li>
         ))}
       </ol>
@@ -111,3 +109,28 @@ function App() {
 }
 
 export default App;
+
+  {/* {editItem?.id !== item.id &&
+              item.description
+            }
+            {editItem?.id !== item.id &&
+              item.amount
+            }
+            {editItem?.id === item.id &&
+              <form onSubmit={update}>
+                <input value={editDescription} onChange={e => setEditDescription(e.target.value)} />
+                <input value={editAmount} onChange={e => setEditAmount(e.target.value)} />
+                <button>Add</button>
+                <button type="button" onClick={() => setEditedItem(null)}>Cancel</button>
+              </form>
+            }
+            <a href="#" className="delete" onClick={() => remove(item.id)}>
+              Delete
+            </a>&nbsp;
+            {editItem === null &&
+              <a className="edit" onClick={() => setEditedItem(item)} href="#">
+                Edit
+              </a>
+            } */}
+
+            
